a6b1726e9c88b6ce4e318d2e7d9796e3b07ed602
Rakib Hossain <rakib.hossain06@gmail.com>
2026-07-21 17:20:25 +0600

    env variables সুরক্ষিত করা

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://jeygimupxuzalqnkeddf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpleWdpbXVweHV6YWxxbmtlZGRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI2NTE4NTQsImV4cCI6MjA5ODIyNzg1NH0.HBs_2nXS7uZsNgyxHspTfhAoz3G2u6UIWde5ufUJK0A';
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
